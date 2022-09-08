6aab203c32ba06a8e9049f88fef20f355d9603b0
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-08 16:05:39 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -15,8 +15,8 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         
-//         options.addArguments("--no-sandbox");
-//         options.addArguments("--disable-setuid-sandbox");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-setuid-sandbox");
 
 //         options.addArguments("--remote-debugging-port=9222");
 
@@ -26,8 +26,8 @@ public class FirstTest {
 //         options.addArguments("start-maximized");
 //         options.addArguments("disable-infobars");
                              
-//         driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+//         driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
