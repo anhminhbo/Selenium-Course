aac35660fc61a4f27a85779adac9294da6bef32a
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-08 23:39:54 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,77 +1,27 @@
 package com.qascript;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
-class FirstTest {
-	 WebDriver driver;
-
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setupTest() {
+public class FirstTest {
+	@Test
+	public void testing() {
+	WebDriver driver;
+	WebDriverManager.chromedriver().setup();
+	
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void teardown() {
-        driver.quit();
-    }
-
-    @Test
-    void test() {
-//         String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
-//         driver.get(sutUrl);
-//         String title = driver.getTitle();
-//         log.debug("The title of {} is {}", sutUrl, title);
-//         assertThat(driver.getTitle()).contains("Selenium WebDriver");
-	driver.get("https://www.google.com");
+	
+        driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
-//         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-	assertThat(driver.getTitle()).contains("Google");
-    }
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));	
+	}
 
-//     @Test
-//     public void OpenBrowser()  {
-// //         WebDriver driver;
-// //         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-// //         ChromeOptions options = new ChromeOptions();
-        
-// //         options.addArguments("--no-sandbox");
-// //         options.addArguments("--disable-setuid-sandbox");
-
-// //         options.addArguments("--remote-debugging-port=9222");
-
-// //         options.addArguments("--disable-dev-shm-using");
-// //         options.addArguments("--disable-extensions");
-// //         options.addArguments("--disable-gpu");
-// //         options.addArguments("start-maximized");
-// //         options.addArguments("disable-infobars");
-                             
-// //         driver = new ChromeDriver(options);
-// //         driver = new ChromeDriver();
-        
-//         ChromeOptions chromeOptions = new ChromeOptions();
-// 		WebDriverManager.chromedriver().setup();
-// 		WebDriver driver = new ChromeDriver(chromeOptions);
-        
-//         driver.get("https://www.google.com");
-//         System.out.println("Title of the page is: " + driver.getTitle());
-//         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-//     }
 }
 
 
