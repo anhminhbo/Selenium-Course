41cd919019ee34a77894cd532e5f052e0558fb0f
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-08 22:47:13 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -5,29 +5,35 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstTest {
 
 
     @Test
     public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
+//         WebDriver driver;
+//         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//         ChromeOptions options = new ChromeOptions();
         
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-setuid-sandbox");
+//         options.addArguments("--no-sandbox");
+//         options.addArguments("--disable-setuid-sandbox");
 
-        options.addArguments("--remote-debugging-port=9222");
+//         options.addArguments("--remote-debugging-port=9222");
 
-        options.addArguments("--disable-dev-shm-using");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-gpu");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
+//         options.addArguments("--disable-dev-shm-using");
+//         options.addArguments("--disable-extensions");
+//         options.addArguments("--disable-gpu");
+//         options.addArguments("start-maximized");
+//         options.addArguments("disable-infobars");
                              
-        driver = new ChromeDriver(options);
+//         driver = new ChromeDriver(options);
 //         driver = new ChromeDriver();
+        
+        ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(chromeOptions);
+        
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
