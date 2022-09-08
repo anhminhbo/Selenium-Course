192b743d1f5acd9335ed12f36f0f50ca4170dce4
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-08 16:03:36 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -15,18 +15,19 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-setuid-sandbox");
+//         options.addArguments("--no-sandbox");
+//         options.addArguments("--disable-setuid-sandbox");
 
-        options.addArguments("--remote-debugging-port=9222");
+//         options.addArguments("--remote-debugging-port=9222");
 
-        options.addArguments("--disable-dev-shm-using");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-gpu");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
+//         options.addArguments("--disable-dev-shm-using");
+//         options.addArguments("--disable-extensions");
+//         options.addArguments("--disable-gpu");
+//         options.addArguments("start-maximized");
+//         options.addArguments("disable-infobars");
                              
-        driver = new ChromeDriver(options);
+//         driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
