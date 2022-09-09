c082f18389245bd95e1ee5a4d23f3157635d51bc
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-09 21:26:17 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -47,6 +47,7 @@ public class FirstTest {
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+	driver.quit();
     }
 
 }
