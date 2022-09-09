eebae25ac929ff32075fcccdbefa55ba31b46dd9
Minh Nguyen <80105245+anhminhbo@users.noreply.github.com>
2022-09-09 21:18:40 +0700

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -10,29 +10,42 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstTest {
-	@Test
-	public void testing() {
-	WebDriver driver;
-	WebDriverManager.chromedriver().setup();
+// 	@Test
+// 	public void testing() {
+// 	WebDriver driver;
+// 	WebDriverManager.chromedriver().setup();
 	
+//         ChromeOptions options = new ChromeOptions();
+// 	options.addArguments("--no-sandbox");
+//         options.addArguments("--headless");
+// 	options.addArguments("--remote-debugging-port=922");
+//         options.addArguments("--disable-setuid-sandbox");
+//         options.addArguments("--disable-infobars");
+//         options.addArguments("--disable-dev-shm-usage");
+//         options.addArguments("--enable-automation");
+		
+		
+//         driver = new ChromeDriver(options);
+	
+//         driver.get("https://www.google.com");
+//         System.out.println("Title of the page is: " + driver.getTitle());
+//         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
+		
+// 	driver.quit();
+// 	}
+	
+	  @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-	options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-	options.addArguments("--remote-debugging-port=922");
-        options.addArguments("--disable-setuid-sandbox");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--enable-automation");
-		
-		
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
-	
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-		
-	driver.quit();
-	}
+    }
 
 }
 
